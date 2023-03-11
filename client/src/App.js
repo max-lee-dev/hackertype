ed80c355b844c6c5849e8593cebdad92420fbdb8
stqrin <maxplee8@gmail.com>
2023-03-11 18:05:52 -0500

    Ready to start Chakra-UI implementation

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import About from './pages/About.js'
 import Leaderboard from './pages/Leaderboard.js'
 import Solutions from './pages/Solutions.js'
 import { Routes, Route } from 'react-router-dom'
+import {
+  ChakraProvider,
+} from '@chakra-ui/react'
 
 
 
@@ -18,16 +21,18 @@ function App() {
   
   return (
    
-    <>
-      <NavBar/>
-        <div className = 'pageContainer'>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/about' element={<About/>} />
-            <Route path='/leaderboard' element={<Leaderboard/>} />
-            <Route path='/solutions' element={<Solutions/>} />
-          </Routes>
-        </div>
+    <>  
+      <ChakraProvider>
+          <NavBar/>
+            <div className = 'pageContainer'>
+              <Routes>
+                <Route path='/' element={<Home/>} />
+                <Route path='/about' element={<About/>} />
+                <Route path='/leaderboard' element={<Leaderboard/>} />
+                <Route path='/solutions' element={<Solutions/>} />
+              </Routes>
+            </div>
+      </ChakraProvider>
     </>
   )
 }
--- client/src/pages/components/CodeSettings.js
+++ client/src/pages/components/CodeSettings.js
@@ -14,70 +14,75 @@ import {
         FormHelperText,
         ModalFooter,
         Button,
+        IconButton,
         Stack
       } from '@chakra-ui/react'
 export default function CodeSettings({
         id, language, isOpen, onOpen, onClose, wordLimit, handleWordLimit, Restart, solutionRange, setId
 }) {
+
+        
   return (
+        
         <div>
+                
                 <Center>
                         <Stack isInline>
-                        <Button onClick={onOpen}>Open</Button>
-                        <Modal isOpen={isOpen} onClose={onClose}>
-                        <ModalOverlay />
-                        <ModalContent>
-                        <ModalHeader>
-                                <ModalCloseButton />
-                        </ModalHeader>
+                                <IconButton colorScheme='yellow' onClick={onOpen} >Open</IconButton>
+                                <Modal isOpen={isOpen} onClose={onClose}>
+                                <ModalOverlay />
+                                <ModalContent>
+                                <ModalHeader>
+                                        <ModalCloseButton />
+                                </ModalHeader>
 
-                        <ModalBody>
-                                
-                                <FormControl>
-                                <FormLabel>Word Limit</FormLabel>
-                                <Input type="text" />
-                                <FormHelperText>
-                                Enter a word limit for solutions
-                                </FormHelperText>
-                                </FormControl>
-                        </ModalBody>
+                                <ModalBody>
+                                        
+                                        <FormControl>
+                                        <FormLabel>Word Limit</FormLabel>
+                                        <Input type="text" />
+                                        <FormHelperText>
+                                        Enter a word limit for solutions
+                                        </FormHelperText>
+                                        </FormControl>
+                                </ModalBody>
+                
+                                <ModalFooter>
+                                        <Button type="submit" form="new-note">
+                                        Submit
+                                        </Button>
+                                </ModalFooter>
+                                </ModalContent>
+                                </Modal>
+                                        <div className = 'maxWordsDiv'>
+                                        
+                                        <Input
+                                        className = 'maxWordsForm' 
+                                        placeholder={`Selecting from ${solutionRange} ${language} solutions`} 
+                                        type='text'
+                                        onChange={(e) => handleWordLimit(e.target.value)}
+                                        />
+                                        
+                                        
+                                        </div>
+                                <Divider orientation='vertical' size='xl' borderColor={'chakra-placeholder-color'}/>
+                                        <div className = 'languageSettings'>
+                                        <Button onClick={() => Restart('C++', wordLimit)}>C++</Button>
+                                        <Button onClick={() => Restart('Java', wordLimit)}>Java</Button>
+                                        <Button onClick={() => Restart('Python', wordLimit)}>Python</Button>
+                                        </div>
+                                <Divider orientation='vertical' size='xl' variant="thick" colorScheme='red'/>
+                                        <div className = 'maxWordsDiv'>
+                                        <Input
+                                        className = 'searchForm' 
+                                        placeholder={'#'}
+                                        onChange={(e) => setId(e.target.value)}
+                                        type='text'
+                                        />
+                                        <button onClick={() => Restart(language, wordLimit, undefined, id)}>Python</button>
 
-                        <ModalFooter>
-                                <Button type="submit" form="new-note">
-                                Submit
-                                </Button>
-                        </ModalFooter>
-                        </ModalContent>
-                        </Modal>
-                        <div className = 'maxWordsDiv'>
-                        
-                        <Input
-                        className = 'maxWordsForm' 
-                        placeholder={`Selecting from ${solutionRange} ${language} solutions`} 
-                        type='text'
-                        onChange={(e) => handleWordLimit(e.target.value)}
-                        />
-                        
-                        
-                        </div>
-                        <Divider orientation='vertical' size='xl' borderrad='blue'/>
-                        <div className = 'languageSettings'>
-                        <button onClick={() => Restart('C++', wordLimit)}>C++</button>
-                        <button onClick={() => Restart('Java', wordLimit)}>Java</button>
-                        <button onClick={() => Restart('Python', wordLimit)}>Python</button>
-                        </div>
-                        <Divider orientation='vertical' size='xl' variant="thick" colorScheme='red'/>
-                        <div className = 'maxWordsDiv'>
-                        <Input
-                        className = 'searchForm' 
-                        placeholder={'#'}
-                        onChange={(e) => setId(e.target.value)}
-                        type='text'
-                        />
-                        <button onClick={() => Restart(language, wordLimit, undefined, id)}>Python</button>
-
-                        <p className='reminder'>Search for a specific solution</p>
-                        </div>
+                                        <p className='reminder'>Search for a specific solution</p>
+                                        </div>
                         </Stack>
                 </Center>
         </div>
