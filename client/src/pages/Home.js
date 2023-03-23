96953561572b9685aa27948c79e5b4df22e13352
stqrin <maxplee8@gmail.com>
2023-03-23 00:01:24 -0400

    🤐 quick no login = no redirect bug fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -145,12 +145,12 @@ function App({user, givenId}) {
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
         if (!givenLanguage) Restart(doc.data().lastLanguage, '');
-        else Restart(givenLanguage, '');
       });
     }
     if (user) getUserSettings().then(() => setLoading(false))
     if (!user) {
-      Restart('Java', '')
+      if (!givenLanguage) Restart('Java', '')
+      else Restart(givenLanguage, '')
       setLoading(false)
     }
   }, [user])
