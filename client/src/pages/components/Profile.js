3f72ff30cd4ce0f08bf4f95c83cbcd2cc9694f2a
stqrin <maxplee8@gmail.com>
2023-03-23 22:06:03 -0400

    chart retry 😪

--- client/src/pages/components/Profile.js
+++ client/src/pages/components/Profile.js
@@ -11,6 +11,7 @@ import {
 } from '@chakra-ui/react'
 
 import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS} from 'chart.js/auto'
 
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
