a4a13f3a90d905a6a4ea225bc0e2b1021a6d9481
maxlee <maxplee8@gmail.com>
2024-06-30 00:03:20 -0400

    🦶 update footer update time

--- client/src/pages/components/ChangelogModal.js
+++ client/src/pages/components/ChangelogModal.js
@@ -48,10 +48,25 @@ export default function ChangelogModal({isChangeOpen, onChangeClose}) {
               <Box paddingBottom="30px">
                 <Text className="mainTextClass mainFont">
                   <Text fontSize="24px" fontWeight={600} color={mainText}>
-                    4/5/2024
+                    6/30/2024
                     <Badge marginBottom="3px" fontSize={"14px"} ml="2" colorScheme="green">
                       new
                     </Badge>
+                  </Text>
+
+                  <Box paddingTop={"15px"}>
+                    <Text color={subtleText}>added: </Text>
+                    <Text> - updated streak leaderboard! (show record streak and daily solution)</Text>
+                  </Box>
+
+                </Text>
+              </Box>
+              <Divider borderColor={subtleText}/>
+              <Box paddingBottom="30px">
+                <Text className="mainTextClass mainFont">
+                  <Text fontSize="24px" fontWeight={600} color={subtleText}>
+                    4/5/2024
+
                   </Text>
 
                   <Box paddingTop={"15px"}>
--- client/src/pages/components/Footer.js
+++ client/src/pages/components/Footer.js
@@ -7,7 +7,7 @@ import FeedbackModal from "./FeedbackModal";
 export default function Footer({config}) {
   const {isOpen: isChangeOpen, onClose: onChangeClose, onOpen: onChangeOpen} = useDisclosure();
   const {isOpen: isFeedbackOpen, onClose: onFeedbackClose, onOpen: onFeedbackOpen} = useDisclosure();
-  const lastUpdateTime = 1712374024000;
+  const lastUpdateTime = 1719720177000;
   // console.log(Date.parse(new Date()));
   const seenUpdate = localStorage.getItem("lastCheckedUpdate") > lastUpdateTime;
 
