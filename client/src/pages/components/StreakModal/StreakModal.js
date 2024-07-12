7fa64abd3e93d132053f598417312a52e8a47da1
maxlee <maxplee8@gmail.com>
2024-07-11 21:17:36 -0400

    🔥51

--- client/src/pages/components/StreakModal/StreakModal.js
+++ client/src/pages/components/StreakModal/StreakModal.js
@@ -118,7 +118,7 @@ export default function StreakModal({
                   </Box>
                   <Text fontSize={'24px'} fontWeight={600} color={mainText} className={'mainFont'}>
 
-                    49
+                    51
                   </Text>
                 </HStack>
 
